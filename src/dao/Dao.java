e030aaa2c8e8707c634cb7576deb9c887714d63e
Administrator <Administrator@DESKTOP-JMMMU1P>
2021-03-27 20:03:15 +0900

    This is commit test message

---
+++ src/dao/Dao.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class Dao {
+	public static void main(String[] args) {
+		System.out.println("this is connection test");
+	}
+}
---
+++ src/dto/Dto.java
@@ -0,0 +1,7 @@
+package dto;
+
+public class Dto {
+	public static void main(String[] args) {
+		System.out.println("this is connection test");
+	}
+}
